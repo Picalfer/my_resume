1d042cfa99ba9667fe3173b2c0b6cb8e81e9d9f4
Arthur Kozhemyakin <trable228@icloud.com>
2024-03-31 16:57:09 +0300

    resume layout

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('button1').addEventListener('click', function () {
+    changeColorPalette('#FADBD8', '#F1948A', '#D98880', '#E6B0AA');
+});
+
+document.getElementById('button2').addEventListener('click', function () {
+    changeColorPalette('#D6EAF8', '#85C1E9', '#5DADE2', '#5499C7');
+});
+
+document.getElementById('button3').addEventListener('click', function () {
+    changeColorPalette('#F2FADB', '#E14631', '#2c2b2b', '#E14631');
+});
+
+document.getElementById('button4').addEventListener('click', function () {
+    changeColorPalette('#E6E6E6', '#33cc33', '#000000', '#ffffff');
+});
+
+function changeColorPalette(bodyColor, titleDecorationColor, leftAreaColor, headingColor) {
+    document.body.style.backgroundColor = bodyColor;
+
+    document.getElementById('title').style.backgroundColor = titleDecorationColor;
+
+    document.getElementById('left-container').style.backgroundColor = leftAreaColor;
+
+    var headings = document.querySelectorAll('.container h3');
+    for (var i = 0; i < headings.length; i++) {
+        headings[i].style.color = headingColor;
+    }
+}
